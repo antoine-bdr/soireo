1fa4a3987202607b4f65faa47220c2fb3e9231b8
antoine0405 <antoine.autocycle@gmail.com>
2025-10-16 15:49:24 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'soireo',
+  webDir: 'www'
+};
+
+export default config;
